8b9f2d42ec4ffb4b69e3ac88df85bc1ec8e69465
PijanyPuchacz <92039869+PijanyPuchacz@users.noreply.github.com>
2025-11-09 21:10:51 -0500

    temp

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::path::PathBuf;
+use structopt::StructOpt;
+use url::{ParseError, Url};
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "options")]
+struct Opt {
+    /// input URL
+    #[structopt(parse(from_str))]
+    url: String,
+
+    // undecided if I will need it
+    /// debug mode
+    #[structopt(short, long)]
+    debug: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opt = Opt::from_args();
+
+    if opt.debug {
+        println!("{:?}", opt);
+    }
+
+    let url = Url::parse(&opt.url);
+
+    let body = reqwest::get(url)
 }
